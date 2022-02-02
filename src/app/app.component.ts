1d69d64a63cdb9f6e3dd7f80e2c692064ff5907a
Teri11 <augustineteri157@gmail.com>
2022-02-02 11:32:31 +0300

    modified component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goal';
+  goals:string[];
+  constructor(){
+    this.goals = ['Watch finding Nemo','Buy cookies','Get new phone case']
+  }
 }
