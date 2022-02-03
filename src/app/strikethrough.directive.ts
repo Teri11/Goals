04aa676fa6637b48ce811ba8768fa8a0e2937731
Teri11 <augustineteri157@gmail.com>
2022-02-03 07:48:44 +0300

    added pipes

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,11 +1,23 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
-constructor(private elem:ElementRef) {
-  this.elem.nativeElement.style.textDecoration='line-through';
- }
 
+constructor(private elem:ElementRef) {}
+
+@HostListener("click") onClicks(){
+  this.textDeco("line-through")
 }
+
+@HostListener("dblclick") onDoubleClicks(){
+  this.textDeco("None")
+}
+private textDeco(action:string){
+  this.elem.nativeElement.style.textDecoration=action;
+  }
+ 
+}
+
+
