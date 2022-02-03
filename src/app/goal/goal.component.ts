e2f8c34125bce05f28605fbfe00a84f9829967e9
Teri11 <augustineteri157@gmail.com>
2022-02-03 22:23:33 +0300

    added bootstrap

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -5,6 +5,7 @@ import { Goal } from '../goal';
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
+  
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
